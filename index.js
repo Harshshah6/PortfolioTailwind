07420a552ab1bc3a1660c562f229192eb63d1350
harshshah6 <harshsandeep23@gmail.com>
2024-01-02 19:22:11 +0530

    feat: using js class to init skillsItems

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+class SkillsItem {
+    data = `<div class="border border-slate-400 rounded-md p-4 flex gap-4 items-center md:min-w-[30%]"><img src="$img" class="w-12 h-12" alt=""><div><h1 class="font-bold text-xl text-center">$title</h1></div></div>`;
+    
+    constructor(imagePath, title) {
+        this.imagePath = imagePath;
+        this.title = title;
+    }
+
+    build() {
+        this.data = this.data.replace('$img', this.imagePath);
+        this.data = this.data.replace('$title', this.title);
+        return this.data;
+    }
+
+}
+
+const skillsContainer = document.getElementById('skillsContainer');
+const skillsLanguages = [
+    new SkillsItem('./images/javalogo.png','JAVA'),
+    new SkillsItem('./images/kotlinlogo.png','KOTLIN'),
+    new SkillsItem('./images/htmllogo.png','HTML'),
+    new SkillsItem('./images/csslogo.png','CSS'),
+    new SkillsItem('./images/javascript.png','JAVASCRIPT'),
+    new SkillsItem('./images/phplogo.png','PHP'),
+    new SkillsItem('./images/cplusplus.png','C++'),
+]
+const skillsFrameworks = [
+    new SkillsItem('./images/tailwind.png', 'TAILWIND'),
+    new SkillsItem('./images/bootstrap.png', 'BOOTSTRAP'),
+]
+const skills = skillsLanguages.concat(skillsFrameworks);
+
+skills.forEach(element => skillsContainer.innerHTML += element.build());
+
 let btn_nav = document.getElementById('btn_nav');
 let nav_drawer = document.getElementById('nav_drawer');
 let click = 0;
@@ -20,6 +54,10 @@ const updatePSize = () => {
 }
 updatePSize();
 
+let sec_home = document.getElementById('home');
+let nav_bar = document.getElementById('navbar');
+sec_home.style.height = `${sec_home.clientHeight - nav_bar.clientHeight}px`;
+
 const intersectionObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -31,5 +69,4 @@ const intersectionObserver = new IntersectionObserver((entries) => {
 },);
 
 const hiddenElements = document.querySelectorAll(".scroll-hidden")
-
 hiddenElements.forEach((ele)=>intersectionObserver.observe(ele));
